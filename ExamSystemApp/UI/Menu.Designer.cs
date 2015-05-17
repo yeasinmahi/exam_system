c6ee45da6a8b16f6cbaa4c674e31536fa9cfa532
yeasinmahi <yeasinmahi72@gmail.com>
2015-05-17 22:11:56 +0600

    Add menu

--- ExamSystemApp/UI/Menu.Designer.cs
+++ ExamSystemApp/UI/Menu.Designer.cs
@@ -28,13 +28,37 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.teacherButton = new System.Windows.Forms.Button();
+            this.studentButton = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // teacherButton
+            // 
+            this.teacherButton.Location = new System.Drawing.Point(79, 104);
+            this.teacherButton.Name = "teacherButton";
+            this.teacherButton.Size = new System.Drawing.Size(116, 71);
+            this.teacherButton.TabIndex = 0;
+            this.teacherButton.Text = "Teacher";
+            this.teacherButton.UseVisualStyleBackColor = true;
+            this.teacherButton.Click += new System.EventHandler(this.teacherButton_Click);
+            // 
+            // studentButton
+            // 
+            this.studentButton.Location = new System.Drawing.Point(223, 103);
+            this.studentButton.Name = "studentButton";
+            this.studentButton.Size = new System.Drawing.Size(116, 71);
+            this.studentButton.TabIndex = 1;
+            this.studentButton.Text = "Student";
+            this.studentButton.UseVisualStyleBackColor = true;
+            this.studentButton.Click += new System.EventHandler(this.studentButton_Click);
             // 
             // ExamSystem
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(416, 327);
+            this.Controls.Add(this.studentButton);
+            this.Controls.Add(this.teacherButton);
             this.Name = "ExamSystem";
             this.Text = "Menu";
             this.ResumeLayout(false);
@@ -42,6 +66,9 @@
         }
 
         #endregion
+
+        private System.Windows.Forms.Button teacherButton;
+        private System.Windows.Forms.Button studentButton;
     }
 }
 
--- ExamSystemApp/UI/Menu.cs
+++ ExamSystemApp/UI/Menu.cs
@@ -16,5 +16,15 @@ namespace ExamSystemApp
         {
             InitializeComponent();
         }
+
+        private void teacherButton_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void studentButton_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
