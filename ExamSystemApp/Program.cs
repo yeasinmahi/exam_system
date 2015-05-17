b20e23c0329d3b8ab564bf4b297722862cf8b895
yeasinmahi <yeasinmahi72@gmail.com>
2015-05-17 22:08:38 +0600

    Layer Architecture Design

--- ExamSystemApp/Program.cs
+++ ExamSystemApp/Program.cs
@@ -16,7 +16,7 @@ namespace ExamSystemApp
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new ExamSystem());
         }
     }
 }
--- ExamSystemApp/UI/Menu.cs
+++ ExamSystemApp/UI/Menu.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace ExamSystemApp
 {
-    public partial class Form1 : Form
+    public partial class ExamSystem : Form
     {
-        public Form1()
+        public ExamSystem()
         {
             InitializeComponent();
         }
